4120cc944f1a167d4db6735e26d2e740c0d91eba
Herroa <roman.gordov@mail.ru>
2023-10-15 20:56:42 +0700

    pawn pos and scale

--- src/main.cpp
+++ src/main.cpp
@@ -3,22 +3,32 @@
 #include <vector>
 #include "classes.h"
 
-
-
 int main()
 {
     Board B;
     B.start();
 
-    //window init
+    // window init
     sf::RenderWindow window(sf::VideoMode(850, 850), "SFML works!");
     // Load a sprite to display
-    sf::Texture texture;
-    if (!texture.loadFromFile("src/img/board.png"))
+    sf::Texture board_texture;
+    if (!board_texture.loadFromFile("src/img/board.png"))
         return EXIT_FAILURE;
-    sf::Sprite sprite(texture);
-    
-    //window loop
+    sf::Sprite board_sprite(board_texture);
+
+    sf::Texture bp_texture;
+    if (!bp_texture.loadFromFile("src/img/bP.png"))
+        return EXIT_FAILURE;
+    sf::Sprite bp_sprite(bp_texture);
+    bp_sprite.setScale(1.328, 1.328);
+    bp_sprite.setPosition(106.25, 106.25);
+
+    sf::Texture wp_texture;
+    if (!wp_texture.loadFromFile("src/img/wP.png"))
+        return EXIT_FAILURE;
+    sf::Sprite wp_sprite(wp_texture);
+
+    // window loop
     while (window.isOpen())
     {
         sf::Event event;
@@ -29,7 +39,10 @@ int main()
         }
 
         window.clear();
-        window.draw(sprite);
+        window.draw(board_sprite);
+        window.draw(wp_sprite);
+
+        window.draw(bp_sprite);
         window.display();
     }
 
